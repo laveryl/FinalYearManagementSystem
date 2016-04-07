4d1bf3124fceafa1435d3c7ae2a9130eea8a097f
Simon Quigley <simonq80@gmail.com>
2016-04-07 22:43:22 +0100

    added bootstrap to most pages, will need to bundle install

--- Back End/FYMS/db/schema.rb
+++ Back End/FYMS/db/schema.rb
@@ -129,4 +129,11 @@ ActiveRecord::Schema.define(version: 20160407143925) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
 end
