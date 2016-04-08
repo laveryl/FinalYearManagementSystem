c3e263213ad41f4303f2fcabece5dea4f28e5f99
groganco <groganco@tcd.ie>
2016-04-08 14:04:35 +0100

    changed a few small things on the home page

--- Back End/FYMS/db/schema.rb
+++ Back End/FYMS/db/schema.rb
@@ -129,11 +129,4 @@ ActiveRecord::Schema.define(version: 20160407143925) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
 end
